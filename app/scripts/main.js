62528b5eb6f0ba1bfe70253ba3e52ce2d97c5de9
Brittany Pietrzykowski <bpietrz7@gmail.com>
2016-08-09 19:03:56 -0400

    added function to get random fortune

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,12 @@
 console.log('\'Allo \'Allo!');
+
+/* Selecting a random fortune */
+function getRandomFortune(fortunes) {
+  var randomValue = fortunes[Math.floor(Math.random() * fortunes.length)];
+  console.log(randomValue)
+}
+
+
 fetch("fortune.json").then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => getRandomFortune(data))
   .catch(error => console.log("promise error: " + error))
