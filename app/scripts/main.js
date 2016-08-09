c8d12dcf932150081234fd6fe8617aee20a3b769
Brittany Pietrzykowski <brittanypietrzykowski@Pietrzykowski.local>
2016-08-09 17:40:55 -0400

    fetching fortunes in js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,4 @@
 console.log('\'Allo \'Allo!');
+fetch("fortune.json").then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log("promise error: " + error))
