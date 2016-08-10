01477f1059b3259eef5381f57ed2977bb2af12d2
Daniel Gamage <hello@danielgamage.com>
2016-08-10 13:43:55 -0400

    output fortune to element

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,19 @@
 console.log('\'Allo \'Allo!');
 
+const output        = document.querySelector('p.output')
+
 /* Selecting a random fortune */
 function getRandomFortune(fortunes) {
-  var randomValue = fortunes[Math.floor(Math.random() * fortunes.length)];
-  console.log(randomValue)
+  const randomValue = fortunes[Math.floor(Math.random() * fortunes.length)];
+  return randomValue
+}
+
+function onFortunesReady(data) {
+  const randomFortune = getRandomFortune(data)
+  output.textContent = randomFortune
 }
 
 
 fetch("fortune.json").then(response => response.json())
-  .then(data => getRandomFortune(data))
+  .then(data => onFortunesReady(data))
   .catch(error => console.log("promise error: " + error))
