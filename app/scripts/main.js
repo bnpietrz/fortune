e0cac9babe5d0897c9d32b010c7c1972cd85c8f8
Daniel Gamage <hello@danielgamage.com>
2016-08-10 21:54:39 -0400

    use localstorage to set cookies

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,15 +3,36 @@ var dailyFortune;
 const fortuneButton = document.querySelector('button.fortune')
 const output        = document.querySelector('p.output')
 
-fortuneButton.addEventListener('click', () => showFortune(dailyFortune))
+fortuneButton.addEventListener('click', () => clickHandler())
 
 function getRandomFortune(fortunes) {
-  const randomValue = fortunes[Math.floor(Math.random() * fortunes.length)];
-  return randomValue
+  return fortunes[Math.floor(Math.random() * fortunes.length)]
 }
 
 function showFortune(fortune) {
   output.textContent = fortune
+}
+
+function getDate() {
+  // Date.now() gets number of ms since Jan 1 1970, so
+  //              sub secs \  ms since 70  \   ms/sec  \  sec/day
+  const dayInt = Math.floor(  Date.now()   /   1000    /   86400  )
+  return dayInt
+}
+
+function hasVisitedToday() {
+  // localStorage uses strings for values, so parseInt() before comparison
+  return ( parseInt(localStorage.getItem('date')) === getDate() )
+}
+
+function clickHandler() {
+  if (!hasVisitedToday()) {
+    // set cookie
+    localStorage.setItem('fortune', dailyFortune)
+    localStorage.setItem('date', getDate())
+  }
+  // use fortune
+  showFortune(dailyFortune)
 }
 
 function onFortunesReady(data) {
@@ -19,6 +40,20 @@ function onFortunesReady(data) {
   dailyFortune = getRandomFortune(data)
 }
 
-fetch("fortune.json").then(response => response.json())
-  .then(data => onFortunesReady(data))
-  .catch(error => console.log("promise error: " + error))
+function getFortunes() {
+  fetch("fortune.json").then(response => response.json())
+    .then(data => onFortunesReady(data))
+    .catch(error => console.log("promise error: " + error))
+}
+
+// console.log("fortune from local storage: " + localStorage.getItem('fortune'))
+// console.log("date from local storage: " + hasVisitedToday())
+
+if (hasVisitedToday() && localStorage.getItem('fortune')) {
+  console.log('use whats in local storage')
+  fortuneButton.removeAttribute('disabled')
+  dailyFortune = localStorage.getItem('fortune')
+} else {
+  console.log('get new fortune')
+  getFortunes()
+}
